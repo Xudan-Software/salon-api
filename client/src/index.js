30caca375cf9c3471d01b8463aba46e4ee7149ed
ou <xu16@vt.edu>
2021-08-09 00:25:34 +0200

    add navigation bar as requirement in the last step.

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Navbar from "react-bootstrap/Navbar";
+import {Container} from "react-bootstrap";
+
+function NavBar(){
+    return(
+        <Navbar id = "NavBar" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/home">AR Salon and Day Spa Services</Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <NavBar />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
