a916149394389527a47d1ac2dc71184009db08db
ou <xu16@vt.edu>
2021-08-09 23:28:08 +0200

    relocate the styling from index.html to app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
-
-
+import {Container} from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import './App.css';
 
 export default function App() {
-
+    return(
+        <Navbar id = "NavBar" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/home">AR Salon and Day Spa Services</Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
 }
 
 
